56049e8477ca668bd68c385d449b0d1ffb30b8bc
Naman Tamrakar <namantam1@gmail.com>
2021-05-28 02:28:04 +0530

    first commit

---
+++ src/math.js
@@ -0,0 +1,76 @@
+const katex = require("katex");
+
+module.exports.latexify = (string, options) => {
+    const regularExpression = /\$\$[\s\S]+?\$\$|\\\[[\s\S]+?\\\]|\\\([\s\S]+?\\\)|\$[^$\\]*(?:\\.[^$\\]*)*\$/g;
+    const blockRegularExpression = /\$\$[\s\S]+?\$\$|\\\[[\s\S]+?\\\]/g;
+
+    const stripDollars = (stringToStrip) =>
+        stringToStrip[0] === "$" && stringToStrip[1] !== "$"
+            ? stringToStrip.slice(1, -1)
+            : stringToStrip.slice(2, -2);
+
+    const getDisplay = (stringToDisplay) =>
+        stringToDisplay.match(blockRegularExpression) ? "block" : "inline";
+
+    const renderLatexString = (s, t) => {
+        let renderedString;
+        try {
+            // returns HTML markup
+            renderedString = katex.renderToString(s, {
+                ...options,
+                displayMode: t === "block",
+                strict: false,
+                output: "html"
+            });
+        } catch (err) {
+            console.error("couldn`t convert string", s);
+            return s;
+        }
+        return renderedString;
+    };
+
+    const result = [];
+
+    const latexMatch = string.match(regularExpression);
+    const stringWithoutLatex = string.split(regularExpression);
+
+    if (latexMatch) {
+        stringWithoutLatex.forEach((s, index) => {
+            result.push({
+                string: s,
+                type: "text"
+            });
+            if (latexMatch[index]) {
+                result.push({
+                    string: stripDollars(latexMatch[index]),
+                    type: getDisplay(latexMatch[index])
+                });
+            }
+        });
+    } else if (string) {
+        result.push({
+            string,
+            type: "text"
+        });
+    }
+
+    const processResult = (resultToProcess=[]) => {
+        const getSpanFromString = (str) => {
+            const strs = str?.split(/\r\n|\r|\n/)
+            const strsHtml = strs?.map(el => el && `<span>${el}</span>`)
+            return strsHtml?.join("<br/>")
+        }
+
+        const newResult = resultToProcess.map((r) => {
+            if (r.type === "text") {
+                return getSpanFromString(r.string);
+            }
+            return renderLatexString(r.string, r.type);
+        });
+
+        return newResult;
+    };
+
+    // Returns list of spans with latex and non-latex strings.
+    return processResult(result);
+};
---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { latexify } = require("./math");
+const katex = require('katex');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views');
+
+app.get("/", (req, res) => {
+    return res.render("form");
+})
+
+app.post("/", (req, res) => {
+    if (req.body.data) {
+        const dataArr = latexify(req.body.data);
+        return res.render("katex", {katex: dataArr})
+    }
+    return res.redirect("/")
+})
+
+app.get("/ping/", (req, res) => {
+    return res.send("Hello Naman!")
+})
+
+app.post("/api/", (req, res) => {
+    let html;
+    if (req.body.data) {
+        const dataArr = latexify(req.body.data);
+        html = dataArr.join("")
+    }
+
+    return res.send({
+        data: html,
+        code: html ? 1 : 0
+    })
+})
+
+app.listen(3000, () => {
+    console.log("listening on http://localhost:3000")
+})
